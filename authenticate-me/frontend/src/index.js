e694059f29f6ebb9dcc1be9a3888b6894558bab8
Christopher Simpson <simpsonc6@winthropalumni.com>
2023-05-05 14:25:26 -0400

    method 1 done

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -1,11 +1,37 @@
+//frontend/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Import BrowserRouter, Provider, and configureStore
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+// Create a variable to access your store and expose it to the window. 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// Define a Root React functional component that returns the App component wrapped in Redux's Provider and React Router DOM's BrowserRouter provider components.
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+// Call ReactDOM.render function passing in the Root component and the HTML element with the id of "root".
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
